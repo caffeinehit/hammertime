87b80594c0dbb58f3f7355c789ff35fa02bc137e
Alen Mujezinovic <flashingpumpkin@gmail.com>
2011-02-03 10:40:18 +0000

    Version bump

--- setup.py
+++ setup.py
@@ -11,7 +11,7 @@ if sys.version_info[:2] < (2, 6):
 
 setup(
     name='Hammertime',
-    version='0.1.2',
+    version='0.1.3',
 
     description='Time tracking with git.',
     long_description=open('README.rst').read(),
